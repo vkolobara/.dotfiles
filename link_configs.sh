bddf9108fc4dd06c028b0902cb9ed2727ed1c84f
Vinko Kolobara <kolobara.vinko@gmail.com>
2018-07-15 13:55:28 +0200

    fix

--- link_configs.sh
+++ link_configs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-sudo ln -s -t ~/.dotfiles/i3 ~/.config/i3
-sudo ln -s -t ~/.dotfiles/albert ~/.config/albert
-sudo ln -s -t ~/.dotfiles/i3pystatus ~/.config/i3pystatus
-sudo ln -s -t ~/.dotfiles/qutebrowser ~/.config/i3pystatus
-sudo ln -s -t ~/.dotfiles/.fehbg ~/.fehbg
-sudo ln -s -t ~/.dotfiles/.mutt ~/.mutt
-sudo ln -s -t ~/.dotfiles/.newsboat ~/.newsboat
-sudo ln -s -t ~/.dotfiles/.vimrc ~/.vimrc
-sudo ln -s -t ~/.dotfiles/.zshrc ~/.zshrc
-sudo ln -s -t ~/.dotfiles/.password.gpg ~/.password.gpg
-sudo ln -s -t ~/.dotfiles/.Xdefaults ~/.Xdefaults
+ln -sf ~/.dotfiles/i3 ~/.config/i3
+ln -sf ~/.dotfiles/albert ~/.config/albert
+ln -sf ~/.dotfiles/i3pystatus ~/.config/i3pystatus
+ln -sf ~/.dotfiles/qutebrowser ~/.config/qutebrowser
+ln -sf ~/.dotfiles/.fehbg ~/.fehbg
+ln -sf ~/.dotfiles/.mutt ~/.mutt
+ln -sf ~/.dotfiles/.newsboat ~/.newsboat
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.password.gpg ~/.password.gpg
+ln -sf ~/.dotfiles/.Xdefaults ~/.Xdefaults
