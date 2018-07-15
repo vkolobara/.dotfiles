07becc31e49000e63682651d819fd9048c20cee6
Vinko Kolobara <kolobara.vinko@gmail.com>
2018-07-15 13:43:01 +0200

    link qutebrowser

--- link_configs.sh
+++ link_configs.sh
@@ -2,6 +2,7 @@
 sudo ln -s -t ~/.dotfiles/i3 ~/.config/i3
 sudo ln -s -t ~/.dotfiles/albert ~/.config/albert
 sudo ln -s -t ~/.dotfiles/i3pystatus ~/.config/i3pystatus
+sudo ln -s -t ~/.dotfiles/qutebrowser ~/.config/i3pystatus
 sudo ln -s -t ~/.dotfiles/.fehbg ~/.fehbg
 sudo ln -s -t ~/.dotfiles/.mutt ~/.mutt
 sudo ln -s -t ~/.dotfiles/.newsboat ~/.newsboat
